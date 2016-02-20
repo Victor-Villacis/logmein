d1c6fa5f5f9671439f458dbfe18700e3ccc30d78
vicrtor villacis <victor.villacis@outlook.com>
2016-02-20 01:59:08 -0500

    added sequelize, created new sql connections

--- index.js
+++ index.js
@@ -5,42 +5,25 @@ var expressHandleBars = require ('express-handlebars');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var Sequelize = require('sequelize');
-//Declares the app
+//Declares the app with express
 var loginapp = express();
 //Checks the environment port if not use 3000.
 var PORT = process.env.NODE_ENV || 3000;
-
-
 //This sets the handlebars layout themes
 loginapp.engine('handlebars', expressHandleBars({
   defaultLayout:'main'
 }));
-
 loginapp.set('view engine', 'handlebars');
-
 //This sets the body parser which url encodes the url.
 loginapp.use(bodyParser.urlencoded ({
   extended: false
 }));
-
-//Using mysql.createConnection to creare a connecction to the mysql database.
-var connection = mysql.createConnection ({
-  port: 3306,
-  host:'localhost',
-  user:'root',
-  password:'password',
-  database:'logmein_authentication'
-});
-//Connecting to mysql.
-connection.connect();
-
 //This is creating a route and passing the html
 loginapp.get('/', function(req, res){
   res.render('home', {
     msg:req.query.msg    //You need this here in order to make the session work since you are checking if they are authenthicated.
   });
 });
-
 //Using the session here first.
 loginapp.use(session({   //you are calling the variable session to be used by express.
     secret: 'for i 11lsdqwerty',
@@ -50,8 +33,15 @@ loginapp.use(session({   //you are calling the variable session to be used by ex
     saveUninitialized:true,
     resave:false
 }));
-
-
+//New connection to mysql databse
+var connection = new Sequelize('logmein_authentication', 'root', 'password');
+//Model-representation of data.
+var User = connection.define('User', {
+    email:Sequelize.STRING,
+    password:Sequelize.STRING,
+    firstName:Sequelize.STRING,
+    lastname:Sequelize.STRING
+});
 
 /******* Start Regestration Code *******/
 
@@ -59,31 +49,16 @@ loginapp.use(session({   //you are calling the variable session to be used by ex
 loginapp.post('/register', function (req, res) {
   var email = req.body.email;
   var password = req.body.password;
-//This is a registering query command and a query that checks if your account already exist.
-//Checks if users email is already in the database and then escapes.
-  var insertQuery = "INSERT INTO users(email, password) VALUES(?, ?)";
-  var checkQuery = "SELECT * FROM users WHERE email=?" + connection.escape(email);
 
-//Then we must execute the query command.Thus we do the following.
-  connection.query(checkQuery, function(err, results) {
-    if(err){
-      throw err;
-    }
-    //If the user already exist then redirect to a page that tells them they already exist.
-    else if (results.length > 0){
-      res.redirect('/?msg=Already exists');
-      //else they are redirected to a success page.
-    } else { connection.query(insertQuery, [email, password], function(err){
-      if(err) {
-        throw err;
-      }
-      //before being redirected
-      req.session.authenticated = true;
-      //If no error than redirect to success page as you succeded in registering.
-      res.redirect('/success');
-    });
-    }
-  });//End connection.Query(checkQuery)
+  User.create({
+    email: email,
+    password: password
+  }).then(function(result) {
+    res.session.authenticated = user;
+    res.redirect('/success');
+  }).catch(function (err) {
+    throw err;
+  });
 });
 
 /******* End Regestration Code *******/
@@ -94,24 +69,31 @@ loginapp.post('/register', function (req, res) {
 
 //Runnig the login request. Storing users information in a variable.
   loginapp.post('/login', function (req, res) {
+    //Get the email and password from the body and storing it into a variable.
     var email = req.body.email;
     var password = req.body.password;
-//Declaring and running the mysql query. ? is a place holder for users input
-    var checkQuery = "SELECT * FROM users WHERE email = ? AND password = ?";
-    //Run the query.
-    connection.Query(checkQuery, function (err, results) {
-      if(err){
-        throw err;
-      } else if (results.length > 0){
-        //before being redirected
-        req.session.authenticated = true;
-        res.redirect('success')
-      } else {
-        res.redirect('/?msg = You failed at life');
+    //Find a user.
+    User.findOne({
+      WHERE: {
+    //Using the variable  stored information and passing it into mysql.
+        email: email,
+        password: password
       }
-    });//End connection.Query(checkQuery)
+    }).then(function(user) {
+      if(user) {
+        res.redirect('/success');
+        res.session.authenticated = true;
+      } else {
+        res.redirect('/?msg=You failed at life');
+      }
+    }).catch(function(err) {
+      throw err;
+    });
+  });
 
-    //login succesful
+/******* End Log In Code *******/
+
+   //login succesful
   loginapp.get('/success', function (req, res, next) {
     if(req.session.authenticated === true) {
       next();
@@ -119,17 +101,13 @@ loginapp.post('/register', function (req, res) {
       res.redirect("/?msg=You need to be authenticated");
     }
   }, function(req, res) {
-    res.send('You did it my main man')
+    res.send('You did it my main man');
   });
-});
 
-/******* End Log In Code *******/
+ connection.sync({force:true}).then(function() {
+    //This sets the loginapp to listen on port 3000
+    loginapp.listen(PORT, function(){
+      console.log("Listening!!!");
+    });
+  });
 
-
-
-
-
-//This sets the loginapp to listen on port 3000
-loginapp.listen(PORT, function(){
-  console.log("Listening!!")
-});
