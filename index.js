1190abfa822211bdb1f34140550f241792ca3a98
vicrtor villacis <victor.villacis@outlook.com>
2016-02-19 23:52:48 -0500

    Getting error with GET

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //Declares the node modules I need for this app.
 var express = require('express');
-var expressHandleBars = require ('express-handlebars');
 var mysql = require('mysql');
+var expressHandleBars = require ('express-handlebars');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var Sequelize = require('sequelize');
 //Declares the app
 var loginapp = express();
 //Checks the environment port if not use 3000.
@@ -19,22 +20,8 @@ loginapp.set('view engine', 'handlebars');
 
 //This sets the body parser which url encodes the url.
 loginapp.use(bodyParser.urlencoded ({
-  extended:false
+  extended: false
 }));
-
-//Using the session here first.
-loginapp.use(session({   //you are calling the variable session to be used by express.
-    secret: 'for i 11lsdqwerty',
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 365 
-    }, 
-    saveUninitialized:true,
-    resave:false
-}));
-
-
-
-
 
 //Using mysql.createConnection to creare a connecction to the mysql database.
 var connection = mysql.createConnection ({
@@ -54,13 +41,24 @@ loginapp.get('/', function(req, res){
   });
 });
 
+//Using the session here first.
+loginapp.use(session({   //you are calling the variable session to be used by express.
+    secret: 'for i 11lsdqwerty',
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    },
+    saveUninitialized:true,
+    resave:false
+}));
+
+
 
 /******* Start Regestration Code *******/
 
 //This is creating a post route, that is taking in the users name and password and storing it into a variable.
 loginapp.post('/register', function (req, res) {
-  var email = res.body.email;
-  var password = res.body.password;
+  var email = req.body.email;
+  var password = req.body.password;
 //This is a registering query command and a query that checks if your account already exist.
 //Checks if users email is already in the database and then escapes.
   var insertQuery = "INSERT INTO users(email, password) VALUES(?, ?)";
@@ -90,14 +88,16 @@ loginapp.post('/register', function (req, res) {
 
 /******* End Regestration Code *******/
 
+
+
 /******* Start Log In Code *******/
 
 //Runnig the login request. Storing users information in a variable.
   loginapp.post('/login', function (req, res) {
-    var email = res.body.email;
-    var password = res.body.password;
+    var email = req.body.email;
+    var password = req.body.password;
 //Declaring and running the mysql query. ? is a place holder for users input
-    var checkQuery = "SELECT * FROM users WHERE email=? AND password=?";
+    var checkQuery = "SELECT * FROM users WHERE email = ? AND password = ?";
     //Run the query.
     connection.Query(checkQuery, function (err, results) {
       if(err){
